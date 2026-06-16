83acacf0531887391cc5c88fd7b1c5246dae3300
krooseeda <sumaman.ran15@wsk.ac.th>
2026-06-16 03:53:06 +0000

    first01.py

---
+++ first01.py
@@ -0,0 +1,7 @@
+print("Hello world")
+print("-----------")
+#จงเขียนโปรแกรมหาค่าผลบวกของตัวเลข 2 ่จำนวน
+num1 =float(input("enter number1:"))
+num2 =float(input("enter number2:"))
+sum=num1+num2
+print("ผลรวมของตัวเลข 2 จำนวนเท่ากับ",sum)
